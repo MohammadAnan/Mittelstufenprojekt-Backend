f12baa11f3752c771e1a8049211cb810056c3502
oCor <28631874+oCor@users.noreply.github.com>
2019-05-09 10:33:20 +0200

    comment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 var http = require("http");
 
+//comment
+
 //create a server object:
 http
   .createServer(function(req, res) {
-    res.setHeader(
-      "Access-Control-Allow-Origin",
-      "https://mmwny6l98y.codesandbox.io"
-    );
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.write("Hello World"); //write a response to the client
     res.end(); //end the response
   })
