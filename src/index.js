068b1ce7f3d56cc50daf9a7d6564b19d0c5c4c2f
oCor <28631874+oCor@users.noreply.github.com>
2019-05-23 10:21:20 +0200

    Implement an API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-/* var http = require("http");*/
-
 var sqlite3 = require("sqlite3").verbose();
 
 const express = require("express");
@@ -13,30 +11,44 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 app.post("/test-page", function(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.send(JSON.stringify(req.body.test));
+  res.send(req.body.test);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// we need two routes (/run and /all), because some queries return results (SELECT) and others don't (CREATE, UPDATE, INSERT)
 
-/*var HttpDispatcher = require("httpdispatcher");
-var http = require("http");
-var dispatcher = new HttpDispatcher(); //add routing support
+// run an SQL query that doesn't return a result set (CREATE, UPDATE, INSERT)
 
-http
-  .createServer(function(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    dispatcher.dispatch(req, res);
-  })
-  .listen(8080);
+app.post("/run", function(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
-dispatcher.setStatic("/resources");
-dispatcher.setStaticDirname("static");
-
-dispatcher.onGet("/", function(req, res) {
-  //create a server object:
-  var db = new sqlite3.Database("sqlite.db");
+  var db = new sqlite3.Database("database.db");
   db.serialize(function() {
-    db.all("SELECT * FROM user", function(err, rows) {
+    // req.body.sql contains the SQL query from the frontend
+    db.run(req.body.sql, function(err) {
+      if (err != null) {
+        res.end(); // TODO return an appropriate HTTP error code
+        return;
+      }
+      res.end();
+    });
+  });
+
+  db.close();
+});
+
+// run an SQL query that returns a result set (SELECT)
+
+app.post("/all", function(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  var db = new sqlite3.Database("database.db");
+  db.serialize(function() {
+    // req.body.sql contains the SQL query from the frontend
+    db.all(req.body.sql, function(err, rows) {
+      if (err != null) {
+        res.end(); // TODO return an appropriate HTTP error code
+        return;
+      }
       res.write(JSON.stringify(rows));
       res.end();
     });
@@ -45,14 +57,24 @@ dispatcher.onGet("/", function(req, res) {
   db.close();
 });
 
-dispatcher.onGet("/route", function(req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  //console.log(req.url);
-  res.end("Routing is working successfully !");
-});
+/*
 
-dispatcher.onError(function(req, res) {
-  res.writeHead(404);
-  res.end("Error, the URL doesn't exist");
-});
+Try the following in the terminal or in Postman:
+
+rm database.db
+node
+
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("database.db")
+
+db.run("CREATE TABLE Developers(Name CHAR, PRIMARY KEY(Name)")
+db.all("SELECT * from Developers", (err, rows) => console.log(JSON.stringify(rows)))
+db.run("INSERT INTO Developers VALUES('Samah')")
+db.run("INSERT INTO Developers VALUES('Anan')")
+db.run("INSERT INTO Developers VALUES('Anas')")
+db.run("INSERT INTO Developers VALUES('Artur')")
+db.all("SELECT * from Developers", (err, rows) => console.log(JSON.stringify(rows)))
+
 */
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
